2b0cec90fc7e82052620afd0527bbd3af711b96c
myazeez <fatai_azeez06@yahoo.com>
2024-04-02 07:21:01 +0100

    Modified some files

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ MICROBIGGE_FILENAME = "pbp4_ncbi_microbigge.csv"
 MICROBIGGE_OUTPUT_FILENAME = "pbp4_microbigge_output.csv"
 OUTPUT_DATA_DIRECTORY_PATH = "/home/azeez/Documents/who_project/data/"
 EMAIL  = "azeez.fatai@lasu.edu.ng" # Change to your email address.
-UPDATE = True # Change this to True once the fasta files have downloaded from NCBI.
+UPDATE = False # Change this to True once the fasta files have downloaded from NCBI.
 
